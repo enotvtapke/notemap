a13cb721977ec25a8a6b874b8e80694a6370e177
Aleksandr Stupnikov <super-stupnikow@mail.ru>
2022-06-11 17:59:48 +0300

    Init

---
+++ src/main/java/com/enotvtapke/notemap/NotemapApplication.java
@@ -0,0 +1,13 @@
+package com.enotvtapke.notemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotemapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enotvtapke/notemap/NotemapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enotvtapke.notemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotemapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
